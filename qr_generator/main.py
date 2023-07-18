2be2fbd7bf91a6f5f9d1ab5f1d5f4746a55f081e
iamsamabdullov <107039382+iamsamabdullov@users.noreply.github.com>
2023-07-18 09:46:55 +0300

    study
    create qr generator

---
+++ qr_generator/main.py
@@ -0,0 +1,13 @@
+import qrcode
+
+def get_qrcode(url = 'www.vk.com', name = 'default'):
+    qr = qrcode.make(data=url)
+    qr.save(stream=f'{name}.png')
+
+    return f'QR code was created! Open the {name}.png'
+
+def main():
+    print(get_qrcode(url='https://chatbot.theb.ai/#/chat/1002', name='chatbot'))
+
+if __name__ == '__main__':
+    main()
